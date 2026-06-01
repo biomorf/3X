c84564fc020399b8829996832976b0ca2c45e10c
biomorf <biomorf.2017@gmail.com>
2026-06-01 21:41:32 +0700

    panel subdomain

--- play.sh
+++ play.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env sh
 set -ex
 
-ansible-playbook -i hosts --become local.yml 
+#ansible-playbook -i hosts --become local.yml 
 
 
 
-#ansible-playbook -i hosts.ini deploy-3xui.yml --tags inbound_create,connection_info -e acme_certificate_force_update=true
+ansible-playbook -i hosts local.yml \
+    -e acme_certificate_force_update=true \
+
+
+    #--tags acme_certificates \
+ 
+
