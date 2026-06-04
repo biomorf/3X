de8cff4ec3f3f980b8d536f5a1231a98a2e1712f
biomorf <biomorf.2017@gmail.com>
2026-06-05 01:58:26 +0700

    WIP: adopt for proot

--- play.sh
+++ play.sh
@@ -1,11 +1,13 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -ex
 
+export LANG=C.UTF-8
+export LC_ALL=C.UTF-8
+
 #ansible-playbook -i hosts --become local.yml 
+env LANG=C.UTF-8 LC_ALL=C.UTF-8 ansible-playbook -i hosts local.yml -f 1 "$@"
 
-
-
-ansible-playbook -i hosts local.yml \
+#ansible-playbook -i hosts local.yml \
 
 #    -e acme_certificate_force_update=true \
 
