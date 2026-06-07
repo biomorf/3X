32f2794e74456bd3ed3bcad3ce110acecebf4ea8
biomorf <biomorf.2017@gmail.com>
2026-06-07 18:40:42 +0700

    WIP: public panel access

--- play.sh
+++ play.sh
@@ -5,14 +5,20 @@ export LANG=C.UTF-8
 export LC_ALL=C.UTF-8
 
 #ansible-playbook -i hosts --become local.yml 
-env LANG=C.UTF-8 LC_ALL=C.UTF-8 ansible-playbook -i hosts local.yml -f 1 -e panel_api_token="BG7DgmVYIUCXN8kU9lvqhQ8YzI0jeNgJU9CPad9PxZ5x5YRr" "$@"
+env LANG=C.UTF-8 LC_ALL=C.UTF-8 ansible-playbook -i hosts local.yml -f 1 \
+	-e panel_api_token="BG7DgmVYIUCXN8kU9lvqhQ8YzI0jeNgJU9CPad9PxZ5x5YRr" \
+        -e panel_external_access=true \
+	"$@"
+
+
 
 #ansible-playbook -i hosts local.yml \
 
 #    -e acme_certificate_force_update=true \
 #    -e reset_3xui_db=true \
 #      # API-токен панели 3X-UI
-#    -e panel_api_token="BG7DgmVYIUCXN8kU9lvqhQ8YzI0jeNgJU9CPad9PxZ5x5YRr"
+#    -e panel_api_token="BG7DgmVYIUCXN8kU9lvqhQ8YzI0jeNgJU9CPad9PxZ5x5YRr" \
+#    -e panel_external_access=true \
 
     #--tags acme_certificates \
  
